0f16de5aa9a6b26f6d4440cbd2b5431f6a97168d
Julian <JulianSerranomanuel21@gmail.com>
2023-07-17 12:56:23 -0300

    Agregamos menu, Salud app con navigate a la first app y Imc Button a la nueva aplicacion

--- app/src/main/java/com/julianserrano/myapppersonal/MenuActivity.kt
+++ app/src/main/java/com/julianserrano/myapppersonal/MenuActivity.kt
@@ -1,11 +1,30 @@
 package com.julianserrano.myapppersonal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.julianserrano.myapppersonal.firstapp.FirstAppActivity
+import com.julianserrano.myapppersonal.imccalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        btnSaludApp.setOnClickListener{ navigateToSaludApp() }
+        val btnImccApp = findViewById<Button>(R.id.btnImcApp)
+        btnImccApp.setOnClickListener{ navigateToImcApp() }
     }
+
+    private fun navigateToImcApp(){
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+    private fun navigateToSaludApp(){
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
---
+++ app/src/main/java/com/julianserrano/myapppersonal/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.julianserrano.myapppersonal.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.julianserrano.myapppersonal.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
